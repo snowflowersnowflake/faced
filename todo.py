d97c3b654b8ed6904768acc09681d43deb132c11
残念弹幕世界 <33081329+snowflowersnowflake@users.noreply.github.com>
2019-10-24 12:58:32 +0800

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,20 @@
+import cv2
+
+from faced import FaceDetector
+from faced.utils import annotate_image
+
+face_detector = FaceDetector()
+img_path='7.jpg'
+img = cv2.imread(img_path)
+rgb_img = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2RGB)
+thresh=0.5
+# Receives RGB numpy image (HxWxC) and
+# returns (x_center, y_center, width, height, prob) tuples.
+bboxes = face_detector.predict(rgb_img, thresh)
+
+# Use this utils function to annotate the image.
+ann_img = annotate_image(img, bboxes)
+
+# Show the image
+cv2.imshow('image',ann_img)
+cv2.waitKey(0)
